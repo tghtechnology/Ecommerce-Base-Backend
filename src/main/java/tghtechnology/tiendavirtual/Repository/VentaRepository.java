591d07daf88c660457ca4fb037e4d89ea612cfc3
tghtechnology <146979530+tghtechnology@users.noreply.github.com>
2024-04-05 12:55:09 -0500

    fix 2

--- src/main/java/tghtechnology/tiendavirtual/Repository/VentaRepository.java
+++ src/main/java/tghtechnology/tiendavirtual/Repository/VentaRepository.java
@@ -8,14 +8,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import tghtechnology.tiendavirtual.Models.Cliente;
 import tghtechnology.tiendavirtual.Models.Venta;
 public interface VentaRepository extends JpaRepository<Venta, Integer>{
 
 	@Query("SELECT v FROM Venta v WHERE v.estado = true")
     List<Venta> listarPedido(Pageable pageable);
 	
-	@Query("SELECT v FROM Venta v WHERE v.estado = true AND v.id_cliente = :cli_id")
-    List<Venta> listarPorCliente(@Param("cli_id") Integer id_cliente, Pageable pageable);
+	@Query("SELECT v FROM Venta v WHERE v.estado = true AND v.cliente = :cli")
+    List<Venta> listarPorCliente(@Param("cli") Cliente cli, Pageable pageable);
 
     @Query("SELECT v FROM Venta v WHERE v.estado = true AND v.id_venta = :ven_id")
     Optional<Venta> listarUno(@Param("ven_id") Integer idVenta);
