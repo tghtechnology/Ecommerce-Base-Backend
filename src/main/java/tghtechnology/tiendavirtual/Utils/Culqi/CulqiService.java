79622886340dd1ed37f14947cd8e2ef7a332b215
desarrollo <146979530+tghtechnology@users.noreply.github.com>
2024-05-06 15:10:03 -0500

    test

--- src/main/java/tghtechnology/tiendavirtual/Utils/Culqi/CulqiService.java
+++ src/main/java/tghtechnology/tiendavirtual/Utils/Culqi/CulqiService.java
@@ -1,51 +1,45 @@
 package tghtechnology.tiendavirtual.Utils.Culqi;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
-import com.culqi.Culqi;
-
-import lombok.Getter;
-import lombok.Setter;
 
 @Service
 public class CulqiService {
-	
-	@Value("${culqi.secret_key}")
-	@Getter @Setter
-	private String secret_key;
-	
-	@Value("${culqi.public_key}")
-	@Getter @Setter
-	private String public_key;
-	@Getter @Setter
-	private String rsa_key;
-	@Getter @Setter
-	private String rsa_id;
-	
-	private Boolean init = false;
-	
-	private final Culqi culqi = new Culqi();
-	
-	/**
-	 * Obtiene la instancia de Culqi del servicio.<br>
-	 * También inicializa el servicio con las llaves pública
-	 * y privada si es que no lo estuviera ya
-	 * @return La instancia de Culqi del servicio.
-	 */
-	public Culqi getCulqi() {
-		if(!init) initialize();
-		return culqi;
-	}
-	
-	/**
-	 * Inicializa la librería de Culqi con la llave pública
-	 * y privada.
-	 */
-	private void initialize() {
-		Culqi.public_key = public_key;
-		Culqi.secret_key = secret_key;
-		init = true;
-	}
+//	
+//	@Value("${culqi.secret_key}")
+//	@Getter @Setter
+//	private String secret_key;
+//	
+//	@Value("${culqi.public_key}")
+//	@Getter @Setter
+//	private String public_key;
+//	@Getter @Setter
+//	private String rsa_key;
+//	@Getter @Setter
+//	private String rsa_id;
+//	
+//	private Boolean init = false;
+//	
+//	private final Culqi culqi = new Culqi();
+//	
+//	/**
+//	 * Obtiene la instancia de Culqi del servicio.<br>
+//	 * También inicializa el servicio con las llaves pública
+//	 * y privada si es que no lo estuviera ya
+//	 * @return La instancia de Culqi del servicio.
+//	 */
+//	public Culqi getCulqi() {
+//		if(!init) initialize();
+//		return culqi;
+//	}
+//	
+//	/**
+//	 * Inicializa la librería de Culqi con la llave pública
+//	 * y privada.
+//	 */
+//	private void initialize() {
+//		Culqi.public_key = public_key;
+//		Culqi.secret_key = secret_key;
+//		init = true;
+//	}
 
 }
